2b4c6bfbc20f0eca3ec867ba350d5ab61edb0e62
mehrrero <m.herrerovalea@gmail..com>
2025-01-17 09:19:42 +0100

    modified:   backend/main.py

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI, Depends
 from core.config import settings
 from core.routers import api_router
 from typing import Annotated
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import *
 
 app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
 app.include_router(api_router)
@@ -13,4 +13,4 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 @app.get("/")
 def hello_api(token: Annotated[str, Depends(oauth2_scheme)]):
-    return {"msg":"Hello FastAPI🚀"}
+    return {"msg":"By the Power of Grayskull! I have the power!"}
