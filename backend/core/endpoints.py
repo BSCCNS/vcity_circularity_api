f4e28fb2a5e4897d7afd70e30d9cfe37cd93373e
mehrrero <m.herrerovalea@gmail..com>
2025-01-28 11:32:27 +0100

    feat(backend/core/endpoints.py): Added user restrictions in main endpoints
    Now users can only delete and acces to the tasks that they created
    
    Issue 3

--- backend/core/endpoints.py
+++ backend/core/endpoints.py
@@ -1,10 +1,12 @@
 # endpoints.py
 from typing import Annotated
 from fastapi import Depends, APIRouter
-from auth.auth import *
 from schemas import schemas
 import asyncio
 import logging
+from auth.auth import check_token
+import datetime
+import jwt
 
 logger = logging.getLogger('uvicorn.error')
 tasks = {}
@@ -18,25 +20,40 @@ router = APIRouter()
 
 # Endpoint to check tasks running
 @router.get("/tasks")
-async def check_tasks():
+async def check_tasks(token: Annotated[str, Depends(check_token)]):
     '''
-    Checks which tasks are being executed in the backend.
+    Checks which tasks created by the user are being executed in the backend.
+    Parameters:
+        token (str): Authenticaton token of bearer type.
     Return:
-        list[str]: List of the tasks being executed.
+        list[str]: List of the task IDs being executed.
     '''
-    return list(tasks.keys())
+    payload = jwt.decode(token, settings.SECRET_KEY, algorithms=settings.ALGORITHM)
+    user = payload['username']
+
+    list_tasks =[]
+
+    for key in tasks.keys():
+        task_ob = tasks[key]
+        if task_ob['user'] == user:
+            list_tasks.append(key)
+
+    return list_tasks
 
 # Endpoint to run the task 
 @router.post("/run")
-async def run_model(input: schemas.Input_Data):
+async def run_model(input: schemas.InputData, token: Annotated[str, Depends(check_token)]):
     '''
     Starts execution of a model task.
     Parameters:
-        input (JSON): Input parameters for the model (see Input_Data schema).
+        input (JSON): Input parameters for the model (see InputData schema).
+        token (str): Authenticaton token of bearer type.
     Return:
         (JSON): ID of the task.
     '''
-
+    #Check user
+    payload = jwt.decode(token, settings.SECRET_KEY, algorithms=settings.ALGORITHM)
+    user = payload['username']
     task_id = str(uuid.uuid4())  # Generate a unique ID for the task
     logger.info(f'Starting run with task ID: {task_id}')
 
@@ -48,23 +65,33 @@ async def run_model(input: schemas.Input_Data):
             logger.info(f'Run with task ID: {task_id} cancelled')
             raise  # Propagate the cancellation exception
 
+    time = str(datetime.datetime.now())
     task = asyncio.create_task(model_task(task_id))
-    tasks[task_id] = task
-    return {"task_id": task_id}
+    task_ob = schemas.ModelTask(task=task, user=user, time=time)
+    tasks[task_id] = task_ob
+    return {"task_id": task_id, }
 
 
 # Endpoint to stop the task
 @router.delete("/stop/{task_id}")
-async def stop_model(task_id: str):
+async def stop_model(task_id: str, token: Annotated[str, Depends(check_token)]):
     '''
     Stops a running task.
     Parameter:
         task_id (str): ID of the task to stop.
+        token (str): Authenticaton token of bearer type.
     Return:
         (JSON): Confirmation of cancellation.
     '''
+    payload = jwt.decode(token, settings.SECRET_KEY, algorithms=settings.ALGORITHM)
+    user = payload['username']
+    task_ob = tasks.get(task_id)
 
-    task = tasks.get(task_id)
+    if not 'user'==task_ob['user']:
+        raise HTTPException(status_code=404, detail="Access forbidden")
+
+
+    task = task_ob['task']
     if not task:
         raise HTTPException(status_code=404, detail="Task not found")
 
--- backend/schemas/schemas.py
+++ backend/schemas/schemas.py
@@ -1,9 +1,15 @@
 from pydantic import BaseModel
 
 
-class Input_Data(BaseModel):
+class InputData(BaseModel):
     prune_measure: int 
     prune_quantiles: int
     h3_zoom: int = 10
     sliders: list[int]
     buffer_walk_distance: int
+
+
+class ModelTask(BaseModel):
+    task: object
+    user: str
+    start_time: str
