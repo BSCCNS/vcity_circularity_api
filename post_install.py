d7a55dcb657bcfdaee78a96654d8a82604b66839
mehrrero <m.herrerovalea@gmail..com>
2025-03-07 13:39:18 +0100

    deleted:    .env 	modified:   .gitignore 	modified:   post_install.py

--- post_install.py
+++ post_install.py
@@ -1,9 +1,11 @@
+import secrets
 import os
 
 if __name__ == "__main__":
 
-    secret_key = os.urandom(32).hex()
+    secret_key = secrets.token_hex(32)
     os.environ["SECRET_KEY"] = secret_key
+
     with open(".env", "a") as env_file:
         env_file.write(f"SECRET_KEY={secret_key}\n")
 
