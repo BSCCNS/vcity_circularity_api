1173e06dd335bf7d7fdba06a28085b69b915d97c
Patricio Reyes <pareyesv@morty>
2025-03-16 23:28:58 +0100

    chore: update settings
    - add TOLERANCE
    - dynamic version

--- src/api_sk/core/config.py
+++ src/api_sk/core/config.py
@@ -1,18 +1,19 @@
-# config.py
-
-from api_sk.data.fake_db import users_db
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
+
+from api_sk.__version__ import __version__
+from api_sk.data.fake_db import users_db
+
 # Setting for the API
 load_dotenv()
 
+
 class Settings:
-    PROJECT_NAME: str = "Skeletor: a barebones API using FastAPI"
-    DESCRIPTION: str = (
-        "This is a basic API with standard funcionalities that will serve as a starting point to build a full API for a project."
-    )
-    CONTACT: dict[str] = {"name": "M. Herrero", "e-mail": "mherrero@bsc.es"}
-    PROJECT_VERSION: str = "1.0"
+    PROJECT_NAME: str = "vCity-circular-api"
+    DESCRIPTION: str = "Circular index API for the vCity project."
+    CONTACT: dict[str, str] = {"name": "M. Herrero", "e-mail": "mherrero@bsc.es"}
+    PROJECT_VERSION: str = __version__
     USERS_DB = users_db
 
     # Scopes for user authorization
@@ -24,5 +25,9 @@ class Settings:
     SECRET_KEY = os.environ["SECRET_KEY"]
     ALGORITHM = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES = 15
+
+    # weighted sum tolerance
+    TOLERANCE = os.getenv("TOLERANCE", 0.001)
+
 
 settings = Settings()
